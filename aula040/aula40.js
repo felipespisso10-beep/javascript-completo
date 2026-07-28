dd586068cf4fb5217a6c39d673fded1389b8ef92
felipespisso10-beep <felipe.spisso10@gmail.com>
2026-07-28 18:07:32 -0300

    atualizacoes

--- aula040/aula40.js
+++ aula040/aula40.js
@@ -1,10 +1,14 @@
 const caixa1=document.querySelector("#caixa1")
 const btn_c=[...document.querySelectorAll(".curso")]
 const c1_2=document.querySelector("#c1_2")
+const cursos=["HTML1","HTML2","HTML3","HTML4","HTML5","HTML6",]
 
-const novoElemento=document.createElement("div")
-novoElemento.setAttribute("Id","c7")
-novoElemento.setAttribute("class","curso c1")
-novoElemento.innerHTML="Reactnative"
+cursos.map((el)=> {
+    const novoElemento=document.createElement("div")
+    novoElemento.setAttribute("Id","c7")
+    novoElemento.setAttribute("class","curso c1")
+    novoElemento.innerHTML=el
+    caixa1.appendChild(novoElemento)
+})
 
-caixa1.appendChild(novoElemento)
+ 
