ddf647eafdc83b3f40f720fb3c2642a9d579d293
felipespisso10-beep <felipe.spisso10@gmail.com>
2026-07-30 12:00:12 -0300

    atualizacoes

---
+++ aula043/aula43.js
@@ -0,0 +1,38 @@
+const caixacursos=document.querySelector("#caixacursos")
+const btn_c=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
+const cursos=["HTML", "CSS", "JavaScript", "PHP", "React", "MySQL", "ReactNative"]
+const btncursoselecionado=document.getElementById("btncursoselecionado")
+
+cursos.map((el,chave)=>{
+    const novoelemento=document.createElement("div")
+    novoelemento.setAttribute("id","c"+chave)
+    novoelemento.setAttribute("class", "curso c1")
+    novoelemento.innerHTML=el
+
+    const comandos=document.createElement("div")
+    comandos.setAttribute("class","comandos")
+
+    const rb=document.createElement("input")
+    rb.setAttribute("type", "radio")
+    rb.setAttribute("name", "rb_curso")
+
+    comandos.appendChild(rb)
+
+    novoelemento.appendChild(comandos)
+
+    caixacursos.appendChild(novoelemento)
+})
+
+btncursoselecionado.addEventListener("click", (evt)=>{
+    const todosradios=[...document.querySelectorAll("input[type=radio]")]
+    const radioselecionados=todosradios.filter((ele)=>{
+        return ele.checked
+    })
+
+    radioselecionados=radioselecionados[0]
+    const cursoselecionado=radioselecionados.parentNode. 
+
+    console.log(todosradios)
+    console.log(cursoselecionado)
+})
