aa4ce21b3f9605013a69fad7775b7b3f58a88086
felipespisso10-beep <felipe.spisso10@gmail.com>
2026-08-04 11:58:01 -0300

    atualizacoes

--- aula058/aula58.js
+++ aula058/aula58.js
@@ -1,17 +1,31 @@
 class Carro{
-    constructor(pnome){
+    constructor(pnome,ptipo){
         this.nome=pnome
+        if(ptipo==1){
+            this.tipo="Esportivo"
+            this.velmax=300        
+        } else if(ptipo==2){
+            this.tipo="Utilitário"
+            this.velmax=100
+        }else if(ptipo==3){
+            this.tipo="Passeio"
+            this.velmax=160
+        }else{
+            this.tipo="Militar"
+            this.velmax=180
+        }
+    }
+
+    info(){
+        console.log("Nome.: " + this.nome)
+        console.log("Tipo.: " + this.tipo)
+        console.log("V.Max.: " + this.velmax)
     }
 }
 
-let c1=new Carro(10)
-let c2=new Carro("Asdervardson")
-let c3=new Carro("Bre")
+let c1=new Carro("Rapidão",1)
+let c2=new Carro("Super Luxo", 2)
+let c3=new Carro("Bombadão", 4)
+let c4=new Carro("arrego tudo", 3)
 
-console.log(p1,nome)
-console.log(p1,nome)
-console.log(p1,nome)
-
-console.log(p1.nome)
-console.log(p2.nome)
-console.log(p3.nome)
+c1.info()
