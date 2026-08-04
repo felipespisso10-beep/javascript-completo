1bdf08f0bedd7d6a1ae9069ee1c376d1eed998de
felipespisso10-beep <felipe.spisso10@gmail.com>
2026-08-04 10:21:11 -0300

    atualizacoes

---
+++ aula058/aula58.js
@@ -0,0 +1,17 @@
+class Carro{
+    constructor(pnome){
+        this.nome=pnome
+    }
+}
+
+let c1=new Carro(10)
+let c2=new Carro("Asdervardson")
+let c3=new Carro("Bre")
+
+console.log(p1,nome)
+console.log(p1,nome)
+console.log(p1,nome)
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
